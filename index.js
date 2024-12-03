4da319213691c4fc114ae38d977e767ec52d033e
Gaurav Dubey <dubeygaurav0506@gmail.com>
2024-12-04 03:32:00 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let requestCount = 0;
+
+app.get('/', (req, res) => {
+    requestCount++;
+    console.log(`Request Count: ${requestCount}`);
+    console.log('Query Params:', req.query);
+    res.send('GET request received');
+});
+
+app.post('/', (req, res) => {
+    requestCount++;
+    console.log(`Request Count: ${requestCount}`);
+    console.log('Request Body:', req.body);
+    res.send('POST request received');
+});
+
+app.put('/', (req, res) => {
+    requestCount++;
+    console.log(`Request Count: ${requestCount}`);
+    console.log('Request Body:', req.body);
+    res.send('PUT request received');
+});
+
+app.get('/reqCount', (req, res) => {    
+    res.send(`Request Count: ${requestCount}`);
+});
+
+app.post('/reset', (req, res) => {
+    requestCount = 0;
+    res.send('Request Count has been reset');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+module.exports = app;
